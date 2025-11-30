e90c9534712f2afdb12c916f179faf58020a53b3
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-30 23:29:30 +0200

    add this to my repository

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,6 +3,18 @@ export default function Input({ label, textarea, ...props}) {
         <label className="text-sm font-bold uppercase text-stone-500">
             {label}
         </label>
-        {textarea ? <textarea ClassName="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props}/> : <input {...props}/>} 
+       {
+        textarea ? (
+            <textarea
+            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+            {...props}
+            />
+        ) : (
+            <input
+            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+            {...props}
+            />
+        )
+        }
     </p>
 }
