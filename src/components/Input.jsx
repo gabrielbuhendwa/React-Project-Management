a012699c34167ed43d925573a0e909f37b11dfd2
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-30 23:19:48 +0200

    add this to my repository

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,8 @@
 export default function Input({ label, textarea, ...props}) {
-    return <p>
-        <label>{label}</label>
+    return <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">
+            {label}
+        </label>
         {textarea ? <textarea {...props}/> : <input {...props}/>} 
     </p>
 }
