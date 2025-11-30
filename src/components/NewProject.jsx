843f270c35677b519a03daca91270ac3000593e8
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-30 22:47:05 +0200

    add this to my repository

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,15 +1,17 @@
 import Input from "./Input.jsx";
 
 export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
-        </menu>
-        <div>
-            <Input label="Title"/>
-            <Input label="Description" textarea/>
-            <Input label="Due Date"/>
+    return (
+        <div className="w-[35rem] mt-16">
+            <menu className="flex item-center justify-end gap-4 my-4">
+                <li><button>Cancel</button></li>
+                <li><button>Save</button></li>
+            </menu>
+            <div>
+                <Input label="Title"/>
+                <Input label="Description" textarea/>
+                <Input label="Due Date"/>
+            </div>
         </div>
-    </div>
+    );
 }
