d80e92c041da06d85a0dd7bfd2a2b4f2b74b5d1b
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-11-30 03:29:16 +0200

    add this to my repository

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,7 +1,7 @@
 export default function ProjectsSidebar() {
   return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50">
-      <h2>Your Projects are here</h2>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects are here</h2>
       <div>
         <button>+ Add Project</button>
       </div>
