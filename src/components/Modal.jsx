e352e57eb870d9a9c96a517795fa5fb895d22ca5
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-02 23:28:50 +0200

    styling the interface

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,8 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
+import Button from "./Button.jsx";
+
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
 
     const dialog = useRef();
@@ -14,10 +16,10 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
     });
 
     return createPortal(
-    <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"> 
+    <dialog ref={dialog} className="backdrop:bg-stone-900 backdrop:bg-opacity-50 backdrop:backdrop-blur-sm rounded-md p-6 border-2 border-stone-600 bg-stone-100 text-stone-800"> 
         {children}  
-        <form method="dialog">
-            <button>{buttonCaption}</button>
+        <form method="dialog" className="mt-4 text-right ">
+            <Button>{buttonCaption}</Button>
         </form>
     </dialog>,
     document.getElementById('modal-root')
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -30,9 +30,9 @@ export default function NewProject({ onAdd }) {
   return (
     <>
     <Modal ref={modal} buttonCaption="Okay">
-      <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
-      <p>You forget to enter a value</p>
-      <p>Provide valid value for every field please</p>
+      <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+      <p className="text-stone-600 mb-4">Ohh, looks like you forget to enter a value</p>
+      <p className="text-stone-600 mb-4">we sugggest you to provide valid value for every field please</p>
     </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
