13f95266074995edefdd73ed39154b677015b08e
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-02 23:50:25 +0200

    setting up a new step of the project

---
+++ src/components/SelectedProject.jsx
@@ -0,0 +1,9 @@
+export default function SelectedProject(){
+    return <div>
+        <header>
+            <h1>TITLE</h1>
+            <button>Delete</button>
+        </header>
+        TASKS
+    </div>
+}
