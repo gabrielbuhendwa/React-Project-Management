dcc3f24f72821275280ae09e6dd06110acb0af55
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-03 02:25:39 +0200

    adding projects tasks logic

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,10 @@
+export default function Tasks(){
+    return <section>
+        <h2>Taks</h2>
+        NEW TASK 
+        <p>This project does not have any taks yet.</p>
+        <ul>
+
+        </ul>
+    </section>
+}
