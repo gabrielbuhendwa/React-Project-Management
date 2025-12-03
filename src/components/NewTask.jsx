55f008980fe0080511a7ac7968be98d34a908571
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-03 02:41:43 +0200

    styling the new task part of the project

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,7 +1,7 @@
 export default function NewTask(){
     return 
-    <div>
-        <input type="text"/>
+    <div className="flex items-center gap-4">
+        <input type="text"className="w-64 px-2 py-1 rounded-sm bg-stone-200"/>
         <button>Add Task</button>
     </div>
 }
