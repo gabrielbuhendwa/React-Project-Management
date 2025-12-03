0cc4feebf91222aebb2cb0e118c541a00405456c
gabrielbuhendwa <gabrielbuhendwa400@gmail.com>
2025-12-03 02:28:13 +0200

    styling the project taks part

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,8 @@
 export default function Tasks(){
     return <section>
-        <h2>Taks</h2>
+        <h2 className="text-2xl font-bold text-stone-700 mb-4">Taks</h2>
         NEW TASK 
-        <p>This project does not have any taks yet.</p>
+        <p className="text-stone-800 mb-4">This project does not have any taks yet.</p>
         <ul>
 
         </ul>
